5eabff2efac026d7538326dfc61cefdfe5b9c1f4
Andreas Ågren <bandagr@gmail.com>
2024-12-05 23:22:36 +0100

    03b

--- 03.fsx
+++ 03.fsx
@@ -1,10 +1,43 @@
 ﻿open System.IO
 open System.Text.RegularExpressions
 
-// let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 let input = File.ReadAllText("./03.txt")
 
-Regex.Matches(input, "mul\((\d+),(\d+)\)")
-|> List.ofSeq
-|> List.map (fun m -> int m.Groups[1].Value * int m.Groups[2].Value)
-|> List.sum
+// Part 1
+
+let part1 =
+    Regex.Matches(input, "mul\((\d+),(\d+)\)")
+    |> List.ofSeq
+    |> List.map (fun m -> int m.Groups[1].Value * int m.Groups[2].Value)
+    |> List.sum
+
+// Part 2
+
+type Instruction =
+    | Do
+    | Dont
+    | Mul of int * int
+
+let parse instruction =
+    match instruction with
+    | "do()" -> Do
+    | "don't()" -> Dont
+    | _ when instruction.StartsWith("mul") ->
+        let values = Regex.Match(instruction, "\((\d+),(\d+)\)")
+        Mul ((int values.Groups[1].Value), (int values.Groups[2].Value))
+    | _ -> failwith instruction
+
+let part2 =
+    Regex.Matches(input, "mul\(\d+,\d+\)|do\(\)|don't\(\)")
+    |> List.ofSeq
+    |> List.map (_.ToString() >> parse)
+    |> List.fold
+        (fun (currInstruction, sumVal) newInstruction ->
+            match newInstruction with
+            | Dont
+            | Do -> (newInstruction, sumVal)
+            | Mul (left, right) ->
+                if currInstruction = Do then (currInstruction, sumVal + left * right)
+                else (currInstruction, sumVal))
+        (Do, 0)
+    |> snd
