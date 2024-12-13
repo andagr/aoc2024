e8795f2782bca5192ba326a441c747053c1284b6
Andreas Ågren <bandagr@gmail.com>
2024-12-13 23:26:22 +0100

    04 b

--- 04.fsx
+++ 04.fsx
@@ -2,18 +2,33 @@ open System
 open System.IO
 open System.Text.RegularExpressions
 
-let input = File.ReadAllLines("./04.txt")
+// let input = File.ReadAllLines("./04.txt")
+
+let input =
+    """.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........""" |> _.Split("\n")
+
+type Letter = { Row: int; Col: int; Value: char }
 
 let rows =
     input
-    |> Array.map _.ToCharArray()
+    |> Array.mapi (fun r line -> line.ToCharArray() |> Array.mapi (fun c v -> { Row = r; Col = c; Value = v }))
+
 
 let cols = Array.transpose rows
 
 let stepDiag rowDelta colDelta startRow startCol =
         seq { for s in 0 .. Int32.MaxValue -> (startRow + s * rowDelta, startCol + s * colDelta) }
 
-let leftDiag (grid: char array array) =
+let leftDiag (grid: Letter array array) =
     let rowMax = Array.length grid - 1
     let colMax = Array.length grid.[0] - 1
     let stepUpRight = stepDiag -1 1
@@ -28,7 +43,7 @@ let leftDiag (grid: char array array) =
             |> Array.map (fun (r, c) -> grid.[r].[c])
     |]
 
-let rightDiag (grid: 'a array array) =
+let rightDiag (grid: Letter array array) =
     let rowMax = Array.length grid - 1
     let colMax = Array.length grid.[0] - 1
     let stepDownRight = stepDiag 1 1
@@ -45,9 +60,9 @@ let rightDiag (grid: 'a array array) =
             |> Array.map (fun (r, c) -> grid.[r].[c])
     |]
 
-let countXmas (grid: char array array) =
+let countXmas (grid: Letter array array) =
     grid
-    |> Array.map (fun row -> String(row))
+    |> Array.map (fun line -> String(line |> Array.map _.Value))
     |> Array.sumBy (fun str -> Regex.Count(str, "(?=(XMAS|SAMX))"))
 
 let part1 =
@@ -55,3 +70,30 @@ let part1 =
     countXmas cols +
     (rows |> leftDiag |> countXmas) +
     (rows |> rightDiag |> countXmas)
+
+let findMasCenters (grid: Letter array) =
+    if Array.length grid < 3 then
+        [||]
+    else
+        [| for ci in 1 .. (Array.length grid - 2) do
+               if grid[ci - 1].Value = 'M' && grid[ci].Value = 'A' && grid[ci + 1].Value = 'S' then
+                    yield grid[ci] |]
+
+let leftDiagMasCenters =
+    rows
+    |> leftDiag
+    |> Array.map findMasCenters
+    |> Array.concat
+
+let rightDiagMasCenters =
+    let rightDiagRows =
+        rows
+        |> rightDiag
+    rightDiagRows
+    |> Array.map findMasCenters
+    |> Array.concat
+
+let part2 =
+    leftDiagMasCenters
+    |> Array.filter (fun l -> rightDiagMasCenters |> Array.exists (fun r -> l.Row = r.Row && l.Col = r.Col))
+    |> Array.length
