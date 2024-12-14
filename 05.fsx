762b758e1735b9e822adf7b12570bf9f623982e4
Andreas Ågren <bandagr@gmail.com>
2024-12-14 16:06:50 +0100

    05 b

--- 05.fsx
+++ 05.fsx
@@ -22,6 +22,18 @@ let isCorrectlyOrdered pageSeq =
     |> Array.pairwise
     |> Array.forall (fun (a, b) -> inputOrderingRules |> Array.contains (a, b))
 
-pageSeqs
-|> Array.filter isCorrectlyOrdered
-|> Array.sumBy middlePage
+let part1 =
+    pageSeqs
+    |> Array.filter isCorrectlyOrdered
+    |> Array.sumBy middlePage
+
+let sortByOrderingRules a b =
+    if inputOrderingRules |> Array.contains (a, b) then -1
+    elif inputOrderingRules |> Array.contains (b, a) then 1
+    else 0
+
+let part2 =
+    pageSeqs
+    |> Array.filter (isCorrectlyOrdered >> not)
+    |> Array.map (Array.sortWith sortByOrderingRules)
+    |> Array.sumBy middlePage
