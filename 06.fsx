0a9358dd03259421b1fdf7599e5ed0e4d3b827a8
Andreas Ågren <bandagr@gmail.com>
2024-12-15 16:28:37 +0100

    06 b

--- 06.fsx
+++ 06.fsx
@@ -1,21 +1,6 @@
-open System
 open System.IO
 
 let input = File.ReadAllLines("./06.txt")
-
-// let input =
-//     """
-//     ....#.....
-//     .........#
-//     ..........
-//     ..#.......
-//     .......#..
-//     ..........
-//     .#..^.....
-//     ........#.
-//     #.........
-//     ......#...
-//     """ |> _.Split("\n", StringSplitOptions.RemoveEmptyEntries)
 
 let room = input |> Array.map _.ToCharArray()
 
@@ -31,7 +16,10 @@ type Guard = {
     Direction: Direction
 }
 
-let isGuard char = char = '^' || char = 'v' || char = '<' || char = '>'
+let isGuard char =
+    match char with
+    | '^' | 'v' | '<' | '>' -> true
+    | _ -> false
 let isObstacle char = char = '#'
 let direction char =
     match char with
@@ -52,7 +40,7 @@ let peek guard =
     | Right -> (guard.Row, guard.Col + 1)
     | Down -> (guard.Row + 1, guard.Col)
     | Left -> (guard.Row, guard.Col - 1)
-let move guard =
+let forward guard =
     match guard.Direction with
     | Up -> { guard with Row = guard.Row - 1 }
     | Right -> { guard with Col = guard.Col + 1 }
@@ -60,7 +48,7 @@ let move guard =
     | Left -> { guard with Col = guard.Col - 1 }
 let hasExited guard =
     let row, col = (guard.Row, guard.Col)
-    row < 0 || row >= room.Length || col < 0 || col >= room.[row].Length
+    row < 0 || row >= room.Length || col < 0 || col >= room[row].Length
 
 let guard =
     room
@@ -70,24 +58,50 @@ let guard =
         {
             Row = guardRow
             Col = guardCol
-            Direction = direction room.[guardRow].[guardCol]
+            Direction = direction room[guardRow].[guardCol]
         }
 
 let obstacles =
     [
         for row in 0..room.Length - 1 do
-            for col in 0..room.[row].Length - 1 do
+            for col in 0..room[row].Length - 1 do
                 if isObstacle room[row].[col] then yield (row, col)
     ]
     |> Set.ofList
 
-let rec steps guard acc =
-    if hasExited guard then acc |> List.distinct |> List.length
-    else
-        let acc' = (guard.Row, guard.Col) :: acc
-        let guard' =
-            if obstacles |> Set.contains (peek guard) then guard |> turn |> move
-            else move guard
-        steps guard' acc'
+let rec move obstacles guard =
+    if obstacles |> Set.contains (peek guard) then guard |> turn |> move obstacles
+    else forward guard
 
-steps guard []
+let patrol guard obstacles =
+    let rec patrol' guard obstacles steps =
+        if hasExited guard then Ok steps
+        else
+            let guard' = move obstacles guard
+            if steps |> Set.contains guard' then
+                Error ()
+            else
+                let steps' = steps |> Set.add guard
+                patrol' guard' obstacles steps'
+    patrol' guard obstacles Set.empty
+
+let part1 =
+    patrol guard obstacles
+    |> Result.map (Set.map (fun guard -> (guard.Row, guard.Col)))
+    |> Result.map Set.count
+
+let addObstacleInPath (obstacles: Set<int * int>) (startStep: Guard) (currentStep: Guard)=
+    let nextStep = move obstacles currentStep
+    if (nextStep.Row, nextStep.Col) = (startStep.Row, startStep.Col) || hasExited nextStep then None
+    else Some (nextStep.Row, nextStep.Col)
+
+let part2 =
+    let initialPatrol = patrol guard obstacles |> Result.defaultValue Set.empty
+    initialPatrol
+    |> Seq.choose (addObstacleInPath obstacles guard)
+    |> Seq.choose (fun newObstacle ->
+        match patrol guard (obstacles |> Set.add newObstacle) with
+        | Ok _ -> None
+        | Error () -> Some newObstacle)
+    |> Seq.distinct
+    |> Seq.length
