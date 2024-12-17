8dfdfb81756f6cd0fcbab3c3b9d4f36e39c6ff22
Andreas Ågren <bandagr@gmail.com>
2024-12-17 23:04:01 +0100

    09 a

---
+++ 09.fsx
@@ -0,0 +1,32 @@
+﻿open System.Collections.Generic
+open System.IO
+
+let input = File.ReadAllText("./09.txt") |> _.Trim()
+
+let block pos nbr =
+    Array.init nbr (fun _ -> if pos % 2 = 0 then Some (pos / 2) else None)
+
+let reorg disk =
+    let blockStack =
+        disk
+        |> Array.choose id
+        |> Stack
+
+    [| for i in 0 .. blockStack.Count - 1 ->
+         match disk[i] with
+         | Some b -> b
+         | None -> blockStack.Pop() |]
+
+let disk =
+    input
+    |> _.ToCharArray()
+    |> Array.map (string >> int)
+    |> Array.mapi block
+    |> Array.concat
+    |> Array.map (Option.map int64)
+
+let part1 =
+    disk
+    |> reorg
+    |> Array.mapi (fun i b -> int64 i * b)
+    |> Array.sum
