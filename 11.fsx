664d6abd77fdc835ccf5baf1afef587d20d5837b
Andreas Ågren <bandagr@gmail.com>
2024-12-20 11:29:38 +0100

    11 b

--- 11.fsx
+++ 11.fsx
@@ -8,15 +8,24 @@ let (|IsEvenDigits|_|) stone =
     if length % 2 = 0 then Some [int64 (str.Substring(0, length / 2)); int64 (str.Substring(length / 2, length / 2))]
     else None
 
+let blinkOnce stone =
+    match stone with
+    | 0L -> [1L]
+    | IsEvenDigits v ->  v
+    | v -> [v * 2024L]
+
 let blink times stones =
-    let blinkOnce stone =
-        match stone with
-        | 0L -> [1L]
-        | IsEvenDigits v ->  v
-        | v -> [v * 2024L]
-    let rec blink' times stones =
+    let rec blink' times (stones: (int64 * int64) list) =
         if times = 0 then stones
-        else stones |> List.collect blinkOnce |> blink' (times - 1)
+        else
+            stones
+            |> List.collect (fun (s, c) -> blinkOnce s |> List.map (fun s -> (s, c)))
+            |> List.groupBy fst
+            |> List.map (fun (s, sc) -> (s, sc |> List.sumBy snd))
+            |> blink' (times - 1)
+    let stones = stones |> List.countBy id |> List.map (fun (s, c) -> (s, int64 c))
     blink' times stones
 
-let part1 = blink 25 stones |> List.length
+let part1 = blink 25 stones |> List.sumBy snd
+
+let part2 = blink 75 stones |> List.sumBy snd
